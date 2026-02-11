045a32f9a4edba6b46558d636cd48bdaae6a337d
Jed Rembold <jedediah.r@gmail.com>
2026-02-11 00:34:40 -0800

    Add initial content

---
+++ Prob2.py
@@ -0,0 +1,13 @@
+"""
+Recreating a flag of your choice!
+"""
+
+from pgl import GWindow, GRect, GOval
+
+
+WIDTH = 600
+HEIGHT = 400
+
+gw = GWindow(WIDTH, HEIGHT)
+
+
---
+++ Prob3.py
@@ -0,0 +1,14 @@
+"""
+Tracing out a drunken robot walk
+"""
+
+from pgl import GWindow, GRect, GOval
+import random
+
+
+WIDTH = 800
+HEIGHT = 800
+
+gw = GWindow(WIDTH, HEIGHT)
+
+
